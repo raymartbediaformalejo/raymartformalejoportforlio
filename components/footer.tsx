391176ebd361fa2ce40a4895a600d67f343726a8
Raymart <100412726+raymartbediaformalejo@users.noreply.github.com>
2024-07-17 03:42:08 +0800

    Update footer

--- components/footer.tsx
+++ components/footer.tsx
@@ -4,7 +4,7 @@ export default function Footer() {
   return (
     <footer className="px-4 mb-10 text-center text-gray-500">
       <small className="block mb-2 text-xs">
-        &copy; 2030 Raymart. All rights reserved.
+        &copy; 2024 Raymart. All rights reserved.
       </small>
     </footer>
   );
